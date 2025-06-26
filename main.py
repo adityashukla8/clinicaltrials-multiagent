4ff398ca8c8b2122698f4b1dde5d4215e6715f2e
adityashukla8 <shuklaaditya473@gmail.com>
2025-06-26 12:18:14 +0530

    Revert "added dashboard metrics"
    This reverts commit d820c92daf01b6725275290a25cf423f7118e88d.

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from fastapi import FastAPI
 from fastapi import HTTPException, status
-from fastapi.responses import JSONResponse
 
 from tools.appwrite_get_all_patients import fetch_all_patients, fetch_patient_by_id
 from tools.clinical_trials_match import match_trials
 from tools.appwrite_write_trial_info import fetch_trial_info, fetch_all_trials
+<<<<<<< HEAD
 from tools.appwrite_metrics import get_appwrite_metrics
+=======
+>>>>>>> parent of d820c92 (added dashboard metrics)
 
 from pydantic import BaseModel
 from typing import List, Optional
-
-from ipdb import set_trace as ipdb
 
 app = FastAPI()
 
@@ -113,6 +113,7 @@ def get_all_trials():
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+<<<<<<< HEAD
 
 @app.get("/metrics")
 def get_metrics():
@@ -121,3 +122,5 @@ def get_metrics():
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     return JSONResponse(content=metrics)
+=======
+>>>>>>> parent of d820c92 (added dashboard metrics)
